cf3caeaa1f5c21e8a9134090e6ebc9c007e7733c
Ash Gray <git@ashm.dev>
2023-04-01 03:25:13 +0200

    feat: backend work for testerplus

---
+++ pages/auth.js
@@ -0,0 +1,59 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+import { SupabaseAdmin } from '../lib/supabase-admin';
+
+export async function getServerSideProps(ctx) {
+	await SupabaseAdmin.rpc('increment_page_view', {
+		page_slug: 'pretendo-rickroll',
+	});
+
+	const code = ctx.query.code;
+
+	const token = await fetch('https://discord.com/api/oauth2/token', {
+		method: 'POST',
+		body: new URLSearchParams({
+			client_id: process.env.DC_CLIENTID,
+			client_secret: process.env.DC_CLIENTSECRET,
+			code,
+			grant_type: 'authorization_code',
+			redirect_uri: 'http://localhost:3000/auth',
+			scope: 'identify',
+		}).toString(),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+	}).then(res => res.json()).then(e => e.access_token);
+
+	const userID = await fetch('https://discord.com/api/users/@me', {
+		headers: {
+			'Authorization': `Bearer ${token}`,
+		},
+	}).then(res => res.json()).then(e => e.id);
+
+	const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
+
+	client.once(Events.ClientReady, async c => {
+		try {
+			const guild = await client.guilds.fetch('408718485913468928');
+			const member = await guild.members.fetch('406125028065804289' || userID);
+			const role = guild.roles.cache.get('1091398279998230668');
+			member.roles.add(role);
+		} catch (e) {
+			console.log(e);
+		}
+	});
+
+	client.login(process.env.DC_BOTTOKEN);
+
+	return {
+		redirect: {
+			destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+			permanent: false,
+		},
+	};
+}
+
+export default function Auth() {
+	return null;
+}
+
